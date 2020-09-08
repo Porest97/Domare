95ddbe365cdd41eede3dac0b62080734e9740fb7
Per Orest <per.orest@outlook.com>
2020-09-08 03:17:34 +0200

    Add project files.

---
+++ Data/DomareContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Domare.Models.DataModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Domare.Data
+{
+    public class DomareContext : IdentityDbContext
+    {
+        public DomareContext (DbContextOptions<DomareContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> Person { get; set; }
+
+    }
+}
